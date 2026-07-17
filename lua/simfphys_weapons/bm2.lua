a66f1fdc7c8b64112222988be65ac6fb3b1b0a41
PhilULSCI <60559527+PhilULSCI@users.noreply.github.com>
2026-07-16 20:58:32 -0700

    Attempting to add spread to BMD-2 Autocannon

--- lua/simfphys_weapons/bm2.lua
+++ lua/simfphys_weapons/bm2.lua
@@ -66,7 +66,7 @@ local function cannon_fire(ply,vehicle,shootOrigin,shootDirection)
 		projectile.shootDirection = shootDirection
 		projectile.attacker = ply
 		projectile.attackingent = vehicle
-		projectile.Spread = Vector(0.01,0.01,0.01)
+		projectile.Spread = Vector(0.1,0.1,0.1)
 		projectile.Damage = 500
 		projectile.Force = 50
 		projectile.Size = 3
