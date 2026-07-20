8a361d8c99e9eee46a889ff47874014e66d3fca0
march <106459595+marchc1@users.noreply.github.com>
2026-07-19 20:07:17 -0700

    Formatting changes

--- lua/entities/arctic_avx_atgm/shared.lua
+++ lua/entities/arctic_avx_atgm/shared.lua
@@ -1,51 +1,44 @@
-ENT.Type 				= "anim"
-ENT.Base 				= "base_entity"
-ENT.PrintName 			= "ATGM"
-ENT.Author 				= ""
-ENT.Information 		= ""
-
-ENT.Spawnable 			= true
-
-
 AddCSLuaFile()
 
-ENT.Model = "models/weapons/w_missile_closed.mdl"
-ENT.FuseTime = 30
-ENT.ArmTime = 0.25
-ENT.Ticks = 0
+ENT.Author      = ""
+ENT.Type        = "anim"
+ENT.Base        = "base_entity"
+ENT.PrintName   = "ATGM"
+ENT.Information = ""
+ENT.Spawnable   = false
+ENT.Model       = "models/weapons/w_missile_closed.mdl"
+ENT.FuseTime    = 30
+ENT.ArmTime     = 0.25
+ENT.Ticks       = 0
 
 if SERVER then
+    function ENT:Initialize()
+        self:SetModel(self.Model)
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetSolid(SOLID_VPHYSICS)
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
+        self:DrawShadow(true)
+        local phys = self:GetPhysicsObject()
+        if phys:IsValid() then
+            phys:Wake()
+            phys:SetBuoyancyRatio(0)
+            phys:EnableGravity(false)
+            phys:EnableDrag(false)
+            phys:SetDragCoefficient(0)
+            phys:SetAngleDragCoefficient(0)
+        end
 
-function ENT:Initialize()
-    self:SetModel( self.Model )
-    self:SetMoveType( MOVETYPE_VPHYSICS )
-    self:SetSolid( SOLID_VPHYSICS )
-    self:PhysicsInit( SOLID_VPHYSICS )
-    self:SetCollisionGroup( COLLISION_GROUP_PROJECTILE )
-    self:DrawShadow( true )
-
-    local phys = self:GetPhysicsObject()
-    if phys:IsValid() then
-        phys:Wake()
-        phys:SetBuoyancyRatio(0)
-        phys:EnableGravity( false )
-        phys:EnableDrag( false )
-        phys:SetDragCoefficient( 0 )
-        phys:SetAngleDragCoefficient( 0 )
+        self.MotorSound = CreateSound(self, "weapons/rpg/rocket1.wav")
+        self.MotorSound:Play()
     end
 
-    self.MotorSound = CreateSound( self, "weapons/rpg/rocket1.wav")
-    self.MotorSound:Play()
-end
-
-function ENT:OnRemove()
-    self.MotorSound:Stop()
-end
-
+    function ENT:OnRemove()
+        self.MotorSound:Stop()
+    end
 end
 
 local images_muzzle = {"effects/muzzleflash1", "effects/muzzleflash2", "effects/muzzleflash3", "effects/muzzleflash4"}
-
 local function TableRandomChoice(tbl)
     return tbl[math.random(#tbl)]
 end
@@ -54,28 +47,25 @@ function ENT:Think()
     if CLIENT then
         if self.Ticks % 5 == 0 then
             local emitter = ParticleEmitter(self:GetPos())
-
-            if !self:IsValid() or self:WaterLevel() > 2 then return end
-
+            if not self:IsValid() or self:WaterLevel() > 2 then return end
             local smoke = emitter:Add("particle/particle_smokegrenade", self:GetPos())
-            smoke:SetVelocity( VectorRand() * 25 )
-            smoke:SetGravity( Vector(math.Rand(-5, 5), math.Rand(-5, 5), math.Rand(-20, -25)) )
-            smoke:SetDieTime( math.Rand(2.0, 2.5) )
-            smoke:SetStartAlpha( 255 )
-            smoke:SetEndAlpha( 0 )
-            smoke:SetStartSize( 0 )
-            smoke:SetEndSize( 125 )
-            smoke:SetRoll( math.Rand(-180, 180) )
-            smoke:SetRollDelta( math.Rand(-0.2,0.2) )
-            smoke:SetColor( 20, 20, 20 )
-            smoke:SetAirResistance( 5 )
-            smoke:SetPos( self:GetPos() )
-            smoke:SetLighting( false )
+            smoke:SetVelocity(VectorRand() * 25)
+            smoke:SetGravity(Vector(math.Rand(-5, 5), math.Rand(-5, 5), math.Rand(-20, -25)))
+            smoke:SetDieTime(math.Rand(2.0, 2.5))
+            smoke:SetStartAlpha(255)
+            smoke:SetEndAlpha(0)
+            smoke:SetStartSize(0)
+            smoke:SetEndSize(125)
+            smoke:SetRoll(math.Rand(-180, 180))
+            smoke:SetRollDelta(math.Rand(-0.2, 0.2))
+            smoke:SetColor(20, 20, 20)
+            smoke:SetAirResistance(5)
+            smoke:SetPos(self:GetPos())
+            smoke:SetLighting(false)
             emitter:Finish()
         end
 
         local emitter = ParticleEmitter(self:GetPos())
-
         local fire = emitter:Add(TableRandomChoice(images_muzzle), self:GetPos())
         fire:SetVelocity(self:GetAngles():Forward() * -1000)
         fire:SetDieTime(0.5)
@@ -83,33 +73,26 @@ function ENT:Think()
         fire:SetEndAlpha(0)
         fire:SetStartSize(32)
         fire:SetEndSize(0)
-        fire:SetRoll( math.Rand(-180, 180) )
+        fire:SetRoll(math.Rand(-180, 180))
         fire:SetColor(255, 255, 255)
         fire:SetPos(self:GetPos())
-
         emitter:Finish()
-
         self.Ticks = self.Ticks + 1
     end
 end
 
 function ENT:Detonate()
-    if !self:IsValid() then return end
+    if not self:IsValid() then return end
     local effectdata = EffectData()
-        effectdata:SetOrigin( self:GetPos() )
-
+    effectdata:SetOrigin(self:GetPos())
     if self:WaterLevel() >= 1 then
-        util.Effect( "WaterSurfaceExplosion", effectdata )
+        util.Effect("WaterSurfaceExplosion", effectdata)
     else
-        util.Effect( "Explosion", effectdata)
+        util.Effect("Explosion", effectdata)
     end
 
     local attacker = self
-
-    if self.Owner:IsValid() then
-        attacker = self.Owner
-    end
-
+    if self.Owner:IsValid() then attacker = self.Owner end
     util.BlastDamage(self, attacker, self:GetPos(), 256, 40)
     self:FireBullets({
         Attacker = attacker,
@@ -121,7 +104,6 @@ function ENT:Detonate()
         IgnoreEntity = self,
         Callback = function(att, tr, dmg)
             util.Decal("Scorch", tr.StartPos, tr.HitPos - (tr.HitNormal * 64), self)
-
             dmg:SetDamageType(DMG_BLAST)
         end
     })
